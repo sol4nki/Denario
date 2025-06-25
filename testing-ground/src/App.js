b8d231cdbda502cd7d01d836d11b761a4367217e
yeehaw <75659510+sol4nki@users.noreply.github.com>
2025-06-25 20:55:41 +0530

    added money for homepage
    just experimental rn will expand this later on

--- testing-ground/src/App.js
+++ testing-ground/src/App.js
@@ -1,46 +1,46 @@
-// import logo from './logo.svg';
-import './App.css';
-import Fallback from './adaptive-icon.png'
+  // import logo from './logo.svg';
+  import './App.css';
+  import Fallback from './adaptive-icon.png'
 
-function App({imageSrc = {Fallback},tokenName = "BITCOIN", nickName = "BTC", cost = 0.001, costinc = 0.001}) {
-  return (
-    <div className="App">
-      <div className='tokenDisplay'>
-        <div className='Pic'>
-          <img src={Fallback} alt='icon'></img>
-        </div>
-        <div className='Names'>
-          <p style={{ fontSize: '18px', marginTop: '0px' }}>{tokenName}</p>
-          <p style={{ fontSize: '18px', opacity: '50%' }}>{nickName}</p>
-        </div>
-        <div className='filler'>
+  function App({imageSrc = {Fallback},tokenName = "BITCOIN", nickName = "BTC", cost = 0.001, costinc = 0.001}) {
+    return (
+      <div className="App">
+        <div className='tokenDisplay'>
+          <div className='Pic'>
+            <img src={Fallback} alt='icon'></img>
+          </div>
+          <div className='Names'>
+            <p style={{ fontSize: '18px', marginTop: '0px' }}>{tokenName}</p>
+            <p style={{ fontSize: '18px', opacity: '50%' }}>{nickName}</p>
+          </div>
+          <div className='filler'>
 
+          </div>
+          <div className='Money'>
+            <p style={{ fontSize: '16px', marginTop: '0px' }}>${cost}</p>
+            <p style={{ fontSize: '14px', color: 'green' }}>{costinc}</p>
+          </div>
+          
         </div>
-        <div className='Money'>
-          <p style={{ fontSize: '16px', marginTop: '0px' }}>${cost}</p>
-          <p style={{ fontSize: '14px', color: 'green' }}>{costinc}</p>
-        </div>
-        
+
+
+
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
       </div>
+    );
+  }
 
-
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
-}
-
-export default App;
+  export default App;
