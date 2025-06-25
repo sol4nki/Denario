0e5d6edc90f49854be3953c80d98a529428a63b8
yeehaw <75659510+sol4nki@users.noreply.github.com>
2025-06-25 13:13:09 +0530

    token display component created
    initialized with variables need to use api

--- testing-ground/src/App.js
+++ testing-ground/src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Fallback from './adaptive-icon.png'
 
-function App() {
+function App({imageSrc = {Fallback},tokenName = "BITCOIN", nickName = "BTC", cost = 0.001, costinc = 0.001}) {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className='tokenDisplay'>
+        <div className='Pic'>
+          <img src={Fallback} alt='icon'></img>
+        </div>
+        <div className='Names'>
+          <p style={{ fontSize: '18px', marginTop: '0px' }}>{tokenName}</p>
+          <p style={{ fontSize: '18px', opacity: '50%' }}>{nickName}</p>
+        </div>
+        <div className='filler'>
+
+        </div>
+        <div className='Money'>
+          <p style={{ fontSize: '16px', marginTop: '0px' }}>${cost}</p>
+          <p style={{ fontSize: '14px', color: 'green' }}>{costinc}</p>
+        </div>
+        
+      </div>
+
+
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +38,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
